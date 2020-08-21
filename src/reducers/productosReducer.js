72c777b0dad53bb9643533aa4e0684875e491fe7
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2020-08-21 15:02:13 -0500

    Evaluar types para consulta al server

--- src/reducers/productosReducer.js
+++ src/reducers/productosReducer.js
@@ -2,7 +2,11 @@ import
     {
         AGREGAR_PRODUCTO,
         AGREGAR_PRODUCTO_EXITO,
-        AGREGAR_PRODUCTO_ERROR
+        AGREGAR_PRODUCTO_ERROR,
+
+        COMENSAR_DESCARGA_PRODUCTOS,
+        DESCARGA_PRODUCTOS_EXITO,
+        DESCARGA_PRODUCTOS_ERROR
     } 
 from '../types/index';
 
@@ -17,6 +21,7 @@ export default function( state = initialState, action ){
 
     switch (action.type) {
 
+        case COMENSAR_DESCARGA_PRODUCTOS:
         case AGREGAR_PRODUCTO:
             return{
                 ...state,
@@ -30,12 +35,22 @@ export default function( state = initialState, action ){
                 productos: [...state.productos, action.payload]
             }
         
+        case DESCARGA_PRODUCTOS_ERROR:
         case AGREGAR_PRODUCTO_ERROR:
             return{
                 ...state,
                 loading: false,
                 error: action.payload
             }
+
+        case DESCARGA_PRODUCTOS_EXITO:
+            return{
+                ...state,
+                loading: false,
+                error: null,
+                productos: action.payload
+            }
+       
             
         default:
             return state;
